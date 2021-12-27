7825da98765c9ecdce856460e87d4c500ba693fb
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-12-27 14:32:22 -0500

    set up state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 function App() {
   const API_KEY = "xd7hHM2MCgiMZ7fARqG7fk9xounFX34D";
+  const [giphy, setGiphy] = useState(null);
 
   return <div className="App"></div>;
 }
