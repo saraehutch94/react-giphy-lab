b0f391091b25fcb88345648b81f050a55bd2e703
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-12-27 14:36:43 -0500

    add getGiphy api call function

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ function App() {
   const API_KEY = "xd7hHM2MCgiMZ7fARqG7fk9xounFX34D";
   const [giphy, setGiphy] = useState(null);
 
+  const getGiphy = async () => {
+    const response = await fetch(
+      `api.giphy.com/v1/gifs/random?apikey=${API_KEY}`
+    );
+    const data = await response.json();
+    setGiphy(data);
+  };
+
   return <div className="App"></div>;
 }
 
